1eba1813bd2406d419f95e525ab25d6d401c00c8
serifcolakel <serifcolakel0@gmail.com>
2023-10-22 22:08:45 +0300

    INFO (SERIF) : Variable Services added.

---
+++ src/modules/variable/variable.service.ts
@@ -0,0 +1,47 @@
+import db from "@lib/db";
+import { CreateVariableRequest, UpdateVariableRequest } from "./variable.type";
+import { Variable } from "@prisma/client";
+
+export async function createVariable(data: CreateVariableRequest) {
+  const variable = await db.variable.create({
+    data,
+  });
+
+  return variable;
+}
+
+export async function deleteVariable(id: Variable["id"]) {
+  const variable = await db.variable.delete({
+    where: {
+      id,
+    },
+  });
+
+  return variable;
+}
+
+export async function updateVariable(
+  id: Variable["id"],
+  data: UpdateVariableRequest
+) {
+  const variable = await db.variable.update({
+    where: {
+      id,
+    },
+    data,
+  });
+
+  return variable;
+}
+
+export const getAllVariablesByEnvironmentId = async (
+  environmentId: Variable["environmentId"]
+) => {
+  const variables = await db.variable.findMany({
+    where: {
+      environmentId,
+    },
+  });
+
+  return variables;
+};
