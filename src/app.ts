95c41f1b84ca90c2da58480ac0811240011b0e81
serifcolakel <serifcolakel0@gmail.com>
2023-10-22 19:17:19 +0300

    INFO (SERIF) : @fastify/swagger & @fastify/swagger-ui package and configuration added for swagger documentation.

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import environmentRoutes from "@modules/environment/environment.route";
 import { fastifyEnvOptions } from "@constants";
 import userRoutes from "@modules/user/user.route";
 import { userSchemas } from "@modules/user/user.schema";
+import swagger from "@fastify/swagger";
+import { withRefResolver } from "fastify-zod";
 import server from "@lib/server";
 import { environmentSchemas } from "@modules/environment/environment.schema";
+import { version } from "../package.json";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 server.get("/api/status", async () => {
   return {
@@ -18,8 +22,11 @@ async function start() {
   server.register(fastifyEnv, fastifyEnvOptions);
   await server.after();
 
-  server.register(userRoutes, { prefix: "/api/users" });
-  server.register(environmentRoutes, { prefix: "/api/environments" });
+  server.addHook("preHandler", (req: any, _res, next) => {
+    req.jwt = server.jwt;
+    return next();
+  });
+
   server.register(fastifyJwt, {
     secret: process.env.JWT_SECRET_KEY!,
   });
@@ -32,6 +39,42 @@ async function start() {
     server.addSchema(schema);
   }
 
+  server.register(
+    swagger,
+    withRefResolver({
+      swagger: {
+        info: {
+          title: "Requester API",
+          description:
+            "Building a blazing fast REST API with Node.js, PostgreSQL, Fastify and Swagger",
+          version,
+        },
+        host: "localhost:3000",
+        schemes: ["http"],
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        securityDefinitions: {
+          Bearer: {
+            type: "apiKey",
+            name: "Authorization",
+            in: "header",
+            description:
+              'JWT Authorization header using the Bearer scheme. Example: "Bearer {token}"',
+          },
+        },
+        security: [{ Bearer: [] }],
+      },
+    })
+  );
+
+  server.register(fastifySwaggerUi, {
+    routePrefix: "/doc",
+    staticCSP: true,
+  });
+
+  server.register(userRoutes, { prefix: "/api/users" });
+  server.register(environmentRoutes, { prefix: "/api/environments" });
+
   try {
     await server.listen({
       port: 3000,
@@ -40,7 +83,9 @@ async function start() {
   } catch (err) {
     console.error(err);
     server.log.error(err);
-    process.exit(1);
+    if (process.env.TS_NODE_DEV !== "true") {
+      process.exit(1);
+    }
   }
 }
 
